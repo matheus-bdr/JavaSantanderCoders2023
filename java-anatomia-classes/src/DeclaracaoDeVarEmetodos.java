f307ca39b389a2be7f10f78aed076802fc47a08f
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-21 09:33:48 -0300

    JavaAnatomiaDeClasses3

---
+++ java-anatomia-classes/src/DeclaracaoDeVarEmetodos.java
@@ -0,0 +1,13 @@
+public class DeclaracaoDeVarEmetodos {
+    public static void main(String[] args) {
+        // métodos: tipoRetorno NomeObjetivoNoInfinitivo Parametro
+        String primeiroNome = "Matheus";
+        String segundoNome = "Santos";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do Método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
