6bb04d79eeff6665099491e780a893dd895d1f77
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-21 09:22:55 -0300

    commit

---
+++ java-anatomia-classes/src/Variavel.java
@@ -0,0 +1,10 @@
+public class Variavel {
+    public static void main(String[] args) {
+        String variavel = "variavel";
+        int numero = 14;
+        float numeroQuebado = 13;
+        System.out.println(variavel);
+        System.out.println(numero);
+        System.out.println(numeroQuebado);
+    }
+}
