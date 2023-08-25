4a0a2646d4b66565cdb2085cac16400302b5abe1
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-25 16:55:33 -0300

    copmmitr

--- escopo/src/Escopo.java
+++ escopo/src/Escopo.java
@@ -1,5 +1,15 @@
 public class Escopo {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //comentarios 
+        
+        /*comentario 
+         * 
+         * em 
+         * 
+         * varias 
+         * 
+         * linhas 
+         */
     }
 }
