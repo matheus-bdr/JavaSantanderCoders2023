ed079c0d7fd84e8f74e55b4d0e8aac4b69f56285
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-25 17:12:35 -0300

    commit

---
+++ terminal-e-argumentos/src/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        String nome = args [0];
+        String sobreNome = args [1];
+        System.out.println("ola eu me chamo "+nome+ "meusobrenome é "+sobreNome);
+    }
+}
