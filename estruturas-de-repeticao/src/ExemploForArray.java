73ee56a326137669bd7b206cf6dab30bfe18abc4
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-30 10:15:34 -0300

    commit

---
+++ estruturas-de-repeticao/src/ExemploForArray.java
@@ -0,0 +1,10 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"bdr","bedas","bedao","bedaste"};
+        
+        for(int i = 0; i < alunos.length; i++){
+            System.out.println("o aluno "+ i +" é "+ alunos[i]);
+        }
+
+    }
+}
---
+++ estruturas-de-repeticao/src/ExemploForArray2.java
@@ -0,0 +1,8 @@
+public class ExemploForArray2 {
+    public static void main(String[] args) {
+        String alunos[] = { "bdr", "bedas", "bedao", "bedaste" };
+        for(String aluno : alunos){
+            System.out.println("O nome do Aluno é: "+ aluno);
+        }
+    }
+}
